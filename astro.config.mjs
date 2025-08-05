04d3b59a9eca289ebf8b013bae12d8559ad0e888
Mariano <salguero.ms@gmail.com>
2025-08-04 21:19:19 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-    base: '/de-tin-marin/',
+  base: '/de-tin-marin/',
+  // fuerza rutas relativas (quita la barra inicial de todas las assets)
+  buildOptions: {
+    assets: 'relative',
+  },
 });
